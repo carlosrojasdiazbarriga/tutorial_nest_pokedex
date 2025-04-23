fbfacdbdc99efb298f6e290ac89dacda3ac1d170
koryto <carlos.rojas.diaz.barriga.com>
2025-04-22 23:18:56 -0600

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+## Descripción
+
+Aplicación Pokedex construida con NestJS
+
+## Instalación
+
+1. Clonar el repositorio
+
+```bash
+git clone <URL_DEL_REPOSITORIO>
+cd pokedex
+```
+
+2. Instalar pnpm (si no está instalado)
+
+```bash
+npm install -g pnpm
+```
+
+3. Instalar dependencias
+
+```bash
+pnpm install
+```
+
+4. Configurar base de datos
+
+- Tener MongoDB 8 instalado y en ejecución
+- Crear archivo `.env` basado en el ejemplo `.env.example`
+
+5. Levantar la aplicación
+
+```bash
+pnpm start:dev
+```
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('pokedex')
+    .setDescription('proyecto de prueba mongodb')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+void bootstrap();
