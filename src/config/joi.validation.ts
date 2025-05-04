6e6edac4bbf56fecae9bfe3d1c40f8309694f028
koryto <carlos.rojas.diaz.barriga.com>
2025-05-03 22:31:36 -0600

    fix: actualizar esquema de validación Joi para hacer que MONGODB sea opcional con valor por defecto

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,6 +1,8 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-  MONGODB: Joi.required(),
+  MONGODB: Joi.optional().default(
+    'mongodb://192.168.100.92:27017/nest-pokemon',
+  ),
   PORT: Joi.number().default(3000),
 });
